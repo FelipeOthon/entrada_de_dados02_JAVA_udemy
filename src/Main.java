f9ab97d20d44918ed8c18e1cac0ea025e42b6519
FelipeOthon <felipeosantiago@gmail.com>
2022-10-14 21:36:08 -0300

    enviar solucao nextLine

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        String s1, s2, s3;
+        x = sc.nextInt();
+        sc.nextLine(); //Foi adicionado nextLine para corrigir a quebra de linha pendente que ocorria no s1
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+
+        System.out.println("DADOS DIGITADOS:");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+
+        sc.close();
     }
 }
